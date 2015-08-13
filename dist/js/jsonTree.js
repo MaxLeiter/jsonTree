9cc4a3d92f5a23ec42402093bb5213dcb0b031c8
Max Leiter <maxwell_leiter@priorypanther.com>
2015-08-13 19:38:16 -0400

    more uniform use of brackets

--- dist/js/jsonTree.js
+++ dist/js/jsonTree.js
@@ -1,8 +1,9 @@
 function jsonTree(jsonURL, selector) {
 
 	var element = document.querySelector(selector);
-	if(!element)
+	if(!element) {
 		console.log('jsonTree element not found!');
+	}
 
 	element.classList.add('jsonTree');
 	var request = new XMLHttpRequest();
@@ -11,28 +12,26 @@ function jsonTree(jsonURL, selector) {
 	request.addEventListener('load', function() {
 		var parsed = JSON.parse(request.responseText);
 		element.innerHTML = json2html(parsed);
-
 		top = document.getElementById('top');
 		top.addEventListener('click', function(e) {
 			e.preventDefault();
 			if(e.target && e.target.nodeName == "LI") {
 				toggleClass(e.target, 'selected');
 			}
-
 		});
 		var parents = toArray(document.querySelectorAll(selector + ' li'));
 		parents.forEach(function(ele, i, a){
 			    var filter = toArray(ele.children).filter(function(el) { return el.tagName.toLowerCase() == 'ul'; });
 				if(filter.length > 0) {
-					if (ele.classList)
+					if (ele.classList) {
 						ele.classList.add('parent');
-					else
+					}
+					else {
 						ele.className += ' ' + 'parent';
+					}
 				}
 		});
-
 	});
-
 }
 
 function toArray(o) {
@@ -44,12 +43,13 @@ function json2html(json) {
     html += "<ul id='top'>";
     for (i in json) {
         html += "<li>"+i+": ";
-        if(typeof json[i] === "object") html += json2html(json[i]);
+        if(typeof json[i] === "object") {
+        	html += json2html(json[i]);
+        }
         else html += json[i];
         html += "</li>";
     }
     html += "</ul>";
-
     return html;
 }
 
